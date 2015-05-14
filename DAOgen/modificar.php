f14d2530fb34996f1a9ee6295d24ca94a19b2013
Cristian Franco <cristixn_93@hotmail.com>
2015-05-14 18:27:04 -0500

    se completa el CRUD
    With delete

--- DAOgen/modificar.php
+++ DAOgen/modificar.php
@@ -4,16 +4,26 @@
   
   
   
-  $list=DAOFactory::getActorDAO()->setSQL("SELECT * FROM actor WHERE first_name ='".$_GET["oldName"]."'");
+  //$list=DAOFactory::getActorDAO()->setSQL("SELECT * FROM actor WHERE first_name ='".$_GET["oldName"]."'");
   
+
+  /*$list= DAOFactory::getActorDAO()->queryByFirstName($_GET["oldName"]);
 
   $obj = new Actor();
 
   $obj->setActorId($list[0]->getActorId());
   
   $obj->setFirstName($_GET["newName"]);
+
+  $obj->setLastName("Franco");
+
+  $obj->setLastUpdate(date("Y-m-d H:i:s"));
   
   DAOFactory:: getActorDAO()->update($obj);
 
- header('Location: consulta.php');
-  
+  header('Location: consulta.php');*/
+
+                            
+
+  DAOFactory::getActorDAO()->deleteByFirstName($_GET["oldName"]);
+   header('Location: consulta.php');
